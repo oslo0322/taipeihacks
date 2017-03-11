f1a3118341368d1cd0ad60afcef715bb19b70c73
呂紹淳 <oslo0322@gmail.com>
2017-03-11 15:48:36 +0800

    update

---
+++ fetch_by_v2_api.py
@@ -0,0 +1,59 @@
+import json
+from datetime import datetime, timedelta
+
+from fetching_booking_api import get_booking_api_response
+from fetch_by_search import get_hotels_url_by_id, get_hotels_photo
+
+def get_data_from_auto_complete(text, lang="en"):
+    payload = {'languagecode': lang, "text": text}
+    uri = "/bookings.autocomplete"
+    data = get_booking_api_response(uri, payload)
+    get_cities_id_list = []
+    for i in data:
+        get_cities_id_list.append({
+            "longitude": i["longitude"],
+            "latitude": i["latitude"],
+        })
+    return get_cities_id_list[0]
+
+
+def main(place, checkin, checkout, stars=1, min_review_score=1, min_price=50, max_price=2000):
+    pos = get_data_from_auto_complete(place)
+    payload = {
+        "checkin": checkin,
+        "checkout": checkout,
+        "longitude": pos["longitude"],
+        "latitude": pos["latitude"],
+        "room1": "A,A",
+        "min_price": min_price,
+        "max_price": max_price,
+        "output": "hotel_details",
+        "rows": 1,
+        "stars": stars,
+        "min_review_score": min_review_score,
+        "radius": 50,
+        "order_by": "price",
+        "currency_code": "TWD"
+    }
+    uri = "/getHotelAvailabilityV2"
+    data = get_booking_api_response(uri, payload)
+    if not data["hotels"]:
+        return None
+    hotel_id = data["hotels"][0]["hotel_id"]
+    price = data["hotels"][0]["price"]
+    hotel_name =  data["hotels"][0]["hotel_name"]
+    review_score = data["hotels"][0]["review_score"]
+    photos = get_hotels_photo(hotel_id)[0]
+    url = get_hotels_url_by_id(hotel_id)
+    return {
+        "title": hotel_name,
+        "subtitle": "review_score: {review_score}, price: {price}".format(review_score=review_score,
+                                                                          price=price),
+        "image_url": photos,
+        "hotel_url": url,
+        "hotel_id": hotel_id
+    }
+
+if __name__ == '__main__':
+    print main("Tokyo", "2017-06-02", "2017-06-10", min_price=10)
+    print main("Tokyo", "2017-06-02", "2017-06-10", min_price=40)
--- fetching_booking_api.py
+++ fetching_booking_api.py
@@ -8,6 +8,5 @@ endpoint = "https://distribution-xml.booking.com/json"
 def get_booking_api_response(uri, payload):
     url = endpoint + uri
     result = requests.get(url, params=payload, auth=HTTPBasicAuth('hacker234', '8hqNW6HtfU'))
-    print result.url
     return json.loads(result.content)
 
